46cf14faec380b4cb7398eb20bdd5b52445685b9
nicochanns <nicolas.chalant@efrei.net>
2023-11-22 11:14:43 +0100

    Test discount applier resolved

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -16,12 +16,21 @@ def test_apply_v1():
     da.apply_v1(10, users)
 
     assert len(notified_users) == 1
-    pass
 
 
 def test_apply_v2():
     # TODO: write a test that fails due to the bug in
     # DiscountApplier.apply_v2
+    notified_users =[]
+    class Notifier:
+        def notify(self, user, message):
+            notified_users.append(user)
+
+    n = Notifier()
+    da = DiscountApplier(n)
     
-    
-    pass
+    users = ['Nico', 'Joel']
+
+    da.apply_v2(5, users)
+
+    assert notified_users[1] == 'Joel'

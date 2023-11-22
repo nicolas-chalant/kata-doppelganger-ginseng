e9a2e2b2882c592301d9d3be37f39f48eaf5f0a9
nicochanns <nicolas.chalant@efrei.net>
2023-11-22 12:05:43 +0100

    Test send mail v2 resolved

--- python/tests/test_mail_sender.py
+++ python/tests/test_mail_sender.py
@@ -30,4 +30,34 @@ def test_send_v1():
 
 def test_send_v2():
     # TODO: write a test that fails due to the bug in MailSender.send_v2
-    pass
+    class User :
+        def __init__(self, name, email):
+            self.name = name
+            self.email = email
+
+    class HttpClient:
+        def __init__(self):
+            self.requests = []
+            
+        def post(self, url, request):
+            self.requests.append(request)
+            if len(self.requests) == 1:
+                return Response(503)
+            else:
+                return Response(200)
+
+    class Response:
+        def __init__(self, code):
+            self.code = code
+
+
+    http_client = HttpClient()
+    mail_sender = MailSender(http_client)
+    user = User("Jean", "jean@gmail.com")
+
+    mail_sender.send_v2(user, "message")
+    expected_request = Request("Jean", "jean@gmail.com", "New notification", "message") 
+
+    assert len(http_client.requests) == 2
+    assert http_client.requests[0] == expected_request
+    assert http_client.requests[1] == expected_request
